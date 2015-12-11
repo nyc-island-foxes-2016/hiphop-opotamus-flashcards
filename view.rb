6be16400bd0154e551e24b2afdd77fda1052e9e4
Nicola Beuscher <email": null>
2015-12-11 14:47:29 -0500

    Add basic run interface method in controller

--- view.rb
+++ view.rb
@@ -8,6 +8,7 @@ class View
   end
 
   def display_welcome_message
-    display("Welcome to Hiphop-opotamus Flashcards!")
+    display("Welcome to Hiphop-opotamus Flashcards!
+    To play, just enter the correct term for each definition.")
   end
 end
