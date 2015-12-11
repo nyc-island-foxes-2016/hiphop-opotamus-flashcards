83a24a55466e5072ae4939d18148b7c8dd9eab07
Marie-France Han & Scott Chou <pair+mfhan.scottychou@devbootcamp.com>
2015-12-11 14:59:43 -0500

    Edit card and deck class

--- deck.rb
+++ deck.rb
@@ -1,2 +1,17 @@
 class Deck
+  attr_reader :deck, :empty
+
+  def initialize
+    @deck = []
+    @empty = false
+  end
+
+  def draw_card #shuffles deck and returns/removes the last card
+    @deck.shuffle.pop
+  end
+
+  def empty?
+    @deck.length == 0
+  end
+
 end
