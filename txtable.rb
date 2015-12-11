c6df6ec72abfadd6e6d4727aed89e5ac73b73839
Pete & Zach <pair+roytuesday.zlschatz@devbootcamp.com>
2015-12-11 15:00:45 -0500

    Define method to create question/answer pairs from a file

--- txtable.rb
+++ txtable.rb
@@ -2,14 +2,18 @@ require 'pry'
 
 module Txtable
 
-def generate_array_from_txt(file_path)
+def generate_line_pairs_from_txt(file_path)
   txt_lines = File.read(file_path).join('\n')
+  pair_txt_lines(txt_lines)
 end
 
 Private
 
-def organize_txt_lines(txt_lines)
-  txt_lines.reject{|line| line == ""}
+def pair_txt_lines(txt_lines)
+  line_pairs = []
+  txt_lines = txt_lines.reject{|line| line == ""}
+  txt_lines.each_slice(2) {|pair| line_pairs << pair}
+  line_pairs
 end
 
 end
