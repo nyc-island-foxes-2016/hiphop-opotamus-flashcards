bae643dc803cfba8e609d00cbd303a8d658874f6
Pete & Zach <pair+roytuesday.zlschatz@devbootcamp.com>
2015-12-11 16:40:06 -0500

    Add comparisons to check input against question answers

--- view.rb
+++ view.rb
@@ -8,7 +8,6 @@ class View
   end
 
   def display_welcome_message
-    display("Welcome to Hiphop-opotamus Flashcards!
-    To play, just enter the correct term for each definition.")
+    display("Welcome to Hiphop-opotamus Flashcards!\n    When done playing, type \"exit\" to quit the game.")
   end
 end
