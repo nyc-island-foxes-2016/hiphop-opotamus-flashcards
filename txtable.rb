7d6f56e2aba780b4e14f436320ff68a071db1920
Pete & Zach <pair+roytuesday.zlschatz@devbootcamp.com>
2015-12-11 14:48:47 -0500

    Define methods to read txt file and remove blanks

--- txtable.rb
+++ txtable.rb
@@ -1,6 +1,15 @@
+require 'pry'
 
-module txtable
+module Txtable
 
+def generate_array_from_txt(file_path)
+  txt_lines = File.read(file_path).join('\n')
+end
 
+Private
+
+def organize_txt_lines(txt_lines)
+  txt_lines.reject{|line| line == ""}
+end
 
 end
