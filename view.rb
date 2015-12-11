1836f84ad84d120a90a493b5fa4d5ad02aa89635
Nicola Beuscher <email": null>
2015-12-11 14:33:15 -0500

    Add display, get input, and welcome message to View

--- view.rb
+++ view.rb
@@ -1,3 +1,13 @@
 class View
+  def display(output)
+    puts output
+  end
 
+  def get_input
+    gets.chomp
+  end
+
+  def display_welcome_message
+    display("Welcome to Hiphop-opotamus Flashcards!")
+  end
 end
